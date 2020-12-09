07389c46fe43c0107b5676ce417036a3128d045c
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-12-09 11:44:49 +0300

    Day5

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -4,11 +4,13 @@ pub mod day1;
 pub mod day2;
 pub mod day3;
 pub mod day4;
+pub mod day5;
 
 pub use day1::Day1;
 pub use day2::Day2;
 pub use day3::Day3;
 pub use day4::Day4;
+pub use day5::Day5;
 
 pub trait AocDay {
     fn run<R: BufRead>(reader: R);
@@ -20,6 +22,7 @@ pub fn get_runner<R: BufRead>(day: usize) -> fn(R) {
         2 => Day2::run,
         3 => Day3::run,
         4 => Day4::run,
+        5 => Day5::run,
         _ => todo!("This day is not implemented yet"),
     }
 }
